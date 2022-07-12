f3dd9c47708a8160e4b7c460c93a2fa28cccf016
Ivars Knets <ivars.knets@gmail.com>
2022-07-12 11:04:04 +0300

    added the router

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
-import logo from './assets/logo.svg';
+
+import React, { Component, Suspense } from "react"
+import { BrowserRouter, Switch, Route } from "react-router-dom"
+
 import './App.css';
 import HomePage from './views/home/home';
 import StorePage from './views/store/store'
 
-function App() {
-  return (
-    <div className="App">
-      
-        <HomePage />
-        <StorePage />
-        
-    </div>
-  );
+
+
+class App extends Component {
+  
+  render(){
+    const currentView = window.location.href.split('/'); 
+    
+    console.log(currentView)
+
+    return (
+      <BrowserRouter basename={''} >      
+
+        <Suspense fallback={<h1>LOADING</h1>}>
+          <Switch location={location}> 
+          
+            <Route exact path={[ '/', '/home' ]} component={HomePage}/>
+            <Route exact path={'/store'} component={StorePage} />
+
+          </Switch>
+        </Suspense>
+          
+      </BrowserRouter>
+    )
+  }
 }
 
 export default App;
